bba5e2c3a5da3f3f7680f562ac106a97fe876cc8
Jahon <worldj95@gmail.com>
2019-06-21 15:27:41 -0700

    Dynamically update text

--- App.js
+++ App.js
@@ -12,7 +12,18 @@ import getImageForWeather from './utils/getImageForWeather';
 import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { location: 'San Francisco' };
+  }
+
+  handleUpdateLocation = city => {
+    this.setState({ location: city });
+  };
+
   render() {
+    const { location } = this.state;
+
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
         <ImageBackground
@@ -21,10 +32,10 @@ export default class App extends React.Component {
           imageStyle={styles.image}
         >
           <View style={styles.detailsContainer}>
-            <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+            <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
             <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
             <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-            <SearchInput placeholder="Your city" />
+            <SearchInput placeholder="Search any city" onSubmit={this.handleUpdateLocation} />
           </View>
         </ImageBackground>
       </KeyboardAvoidingView>
@@ -49,6 +60,7 @@ const styles = StyleSheet.create({
   detailsContainer: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: 'rgba(0,0,0,0.1)',
     paddingHorizontal: 20,
   },
--- components/SearchInput.js
+++ components/SearchInput.js
@@ -2,16 +2,41 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 
 export default class SearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+  handleChangeText = text => {
+    this.setState({ text });
+  };
+
+  handleSubmitEditing = () => {
+    const { onSubmit } = this.props;
+    const { text } = this.state;
+
+    if (!text) return;
+
+    onSubmit(text);
+    this.setState({ text: '' });
+  };
+
   render() {
+    const { placeholder } = this.props;
+    const { text } = this.state;
     return (
       <View style={styles.container}>
         <TextInput
           autoCorrect={false}
-          placeholder={this.props.placeholder}
+          placeholder={placeholder}
           placeholderTextColor="white"
           underlineColorAndroid="transparent"
           style={styles.textInput}
           clearButtonMode="always"
+          value={text}
+          onChangeText={this.handleChangeText}
+          onSubmitEditing={this.handleSubmitEditing}
         />
       </View>
     );
